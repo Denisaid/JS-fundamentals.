1e2a619b95d3fadc48f124ba94d8cb8010e125ed
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-25 20:41:58 +0300

    Create partyTime.js

---
+++ Associative Arrays/Associative Arrays-Exercise/partyTime.js
@@ -0,0 +1,43 @@
+function partyTime(input){
+
+    let vipList=[];
+    let regularList=[];
+
+
+    let command=input.shift();
+
+    while(command!=='PARTY'){
+
+        let firstChar=command[0];
+
+        if(isNaN(firstChar)){
+            regularList.push(command);
+        } else {
+            vipList.push(command);
+        }
+        command=input.shift();
+    }
+    let allGUests=vipList.concat(regularList);
+
+    for (const guest of input) {
+        if(allGUests.includes(guest)){
+            allGUests.splice(allGUests.indexOf(guest), 1);
+        }
+    }
+    console.log(allGUests.length);
+    allGUests.forEach(guest=>console.log(guest));
+    
+}
+
+
+partyTime(['7IK9Yo0h',
+'9NoBUajQ',
+'Ce8vwPmE',
+'SVQXQCbc',
+'tSzE5t0p',
+'PARTY',
+'9NoBUajQ',
+'Ce8vwPmE',
+'SVQXQCbc'
+]
+)
