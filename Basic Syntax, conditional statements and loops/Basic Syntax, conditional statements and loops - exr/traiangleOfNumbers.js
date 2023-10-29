dfcbe1b1a495bc4b6177dd8545ded8be004cf939
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-29 08:53:09 +0200

    traiangleOfNumbers

---
+++ Basic Syntax, conditional statements and loops/Basic Syntax, conditional statements and loops - exr/traiangleOfNumbers.js
@@ -0,0 +1,13 @@
+function traiangle(n) {
+  let result = "";
+
+  for (let rows = 1; rows <= n; rows++) {
+    for (let cols = 1; cols <= rows; cols++) {
+      result += `${rows} `;
+    }
+    console.log(result);
+    result = '';
+  }
+}
+
+traiangle(3);
