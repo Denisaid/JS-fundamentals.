c50215ebe41e60527b48cdb942411b6443d38aa6
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-29 08:52:30 +0200

    multiplicationTable

---
+++ Basic Syntax, conditional statements and loops/Basic Syntax, conditional statements and loops - exr/multiplicationTable.js
@@ -0,0 +1,12 @@
+function print(number){
+
+    for (times=1; times<=10; times++){
+    
+        let product=number*times;
+        console.log(`${number} X ${times} = ${product}`);
+    }
+    
+    }
+    
+    
+    print(5);
