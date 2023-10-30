9c3bc7df7f7dd021172f6aa9d82f7e04e828b02a
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-30 20:03:56 +0200

    lowerOrUpper

---
+++ Data Types and Variables/Data Types and Variables - Exercise/lowerOrUpper.js
@@ -0,0 +1,14 @@
+function lowerOrupper(whether){
+let caseResult='';
+
+if (whether===whether.toUpperCase()){
+    caseResult='upper-case';
+} else {
+    caseResult='lower-case';
+}
+console.log(caseResult);
+
+}
+
+
+lowerOrupper('L')
