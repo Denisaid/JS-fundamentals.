efce79e1d5f5b4400c22abc94702ff4904517cf5
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-31 21:31:46 +0200

    commonElements

---
+++ Arrays/Arrays - exercise/commonElements.js
@@ -0,0 +1,18 @@
+function common(firstArray, secondArray){
+
+for (let index=0; index<firstArray.length; index++){
+
+    let currentElement=firstArray[index];
+
+    if (secondArray.includes(currentElement)){
+        console.log(currentElement);
+    }
+}
+
+
+}
+
+
+
+common(['Hey', 'hello', 2, 4, 'Peter', 'e'],
+['Petar', 10, 'hey', 4, 'hello', '2'])
