652642412acd2cc9fe64903e7161352d5ed4a77f
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-31 21:32:30 +0200

    magisSum

---
+++ Arrays/Arrays - exercise/magisSum.js
@@ -0,0 +1,16 @@
+function magis(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== 0) {
+      let prevNumber = Number(arr[i - 1]);
+      for (let j = i; j < arr.length; j++) {
+        let currentNumber = Number(arr[j]);
+        let sum = currentNumber + prevNumber;
+        if (sum === num) {
+          console.log(`${prevNumber} ${currentNumber}`);
+        }
+      }
+    }
+  }
+}
+
+magis([1, 7, 6, 2, 19, 23], 8);
