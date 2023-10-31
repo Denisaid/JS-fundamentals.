d6bd627406121af327062a565bcd987c9e326562
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-10-31 21:32:49 +0200

    maxSequenceOfEqualElements

---
+++ Arrays/Arrays - exercise/maxSequenceOfEqualElements.js
@@ -0,0 +1,27 @@
+function max(arr) {
+  let sequence = 1;
+  let maxSequence = 0;
+  let maxSequenceNum = 0;
+
+  for (let index = 0; index < arr.length; index++) {
+    if (index !== 0) {
+      let currentNumber = Number(arr[index]);
+      let prevNumber = Number(arr[index - 1]);
+
+      if (currentNumber === prevNumber) {
+        sequence++;
+
+        if (sequence > maxSequence) {
+          maxSequence = sequence;
+          maxSequenceNum = currentNumber;
+        }
+      } else {
+        sequence = 1;
+      }
+    }
+  }
+
+  console.log(`${(maxSequenceNum + " ").repeat(maxSequence)}`);
+}
+
+max([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
