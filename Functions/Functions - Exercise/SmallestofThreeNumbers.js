a68be3d7f51f609692fd0e272e743e59afa147ed
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-11-02 20:56:35 +0200

    SmallestofThreeNumbers

---
+++ Functions/Functions - Exercise/SmallestofThreeNumbers.js
@@ -0,0 +1,7 @@
+function smallestofThreeNumbers(firstNumber,secondNumber,thirdNumber){
+return Math.min(firstNumber,secondNumber,thirdNumber);
+}
+console.log(smallestofThreeNumbers(5,3,2));
+
+
+smallestofThreeNumbers(5,3,2)
