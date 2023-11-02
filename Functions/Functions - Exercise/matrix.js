8f181843dac058daf6c48adcdde37d70a9d352de
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-11-02 20:55:50 +0200

    matrix

---
+++ Functions/Functions - Exercise/matrix.js
@@ -0,0 +1,14 @@
+function matrix(n) {
+  function rowGenerator() {
+    let row = "";
+    for (let j = 1; j <= n; j++) {
+      row += `${n} `;
+    }
+    return row;
+  }
+  for (let i = 1; i <= n; i++) {
+    console.log(rowGenerator());
+  }
+}
+
+matrix(3);
