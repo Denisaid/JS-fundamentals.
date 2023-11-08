77b5e7327abf29203ad80956f76fa697508c1eb1
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-11-08 20:42:04 +0200

    modernTimes

---
+++ Text Processing/Text Processing - Exercise/modernTimes.js
@@ -0,0 +1,28 @@
+function modernTimes(input) {
+  let inputArray = input.split(" ");
+
+  for (const word of inputArray) {
+    if (word[0] === "#" && word.length > 1) {
+      let wordsArray = word.split("");
+      wordsArray.shift();
+
+      let wordToPrint = "";
+
+      for (const char of wordsArray) {
+        if (!isNaN(char)) {
+          wordToPrint = "";
+          break;
+        } else {
+          wordToPrint += char;
+        }
+      }
+      if (wordToPrint.length > 0) {
+        console.log(wordToPrint);
+      }
+    }
+  }
+}
+
+modernTimes(
+  "The symbol # is known #variously in English-speaking #regions as the #number sign"
+);
