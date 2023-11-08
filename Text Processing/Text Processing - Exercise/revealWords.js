51e582aac1af2d2bc16fd62ef221b0ad5d28bb5f
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-11-08 20:42:41 +0200

    revealWords

---
+++ Text Processing/Text Processing - Exercise/revealWords.js
@@ -0,0 +1,15 @@
+function revealWords(words, text) {
+  let wordsArray = words.split(", ");
+
+  for (const word of wordsArray) {
+    let wordsFromStars = "*".repeat(word.length);
+    text = text.replace(wordsFromStars, word);
+  }
+
+  console.log(text);
+}
+
+revealWords(
+  "great",
+  "softuni is ***** place for learning new programming languages"
+);
